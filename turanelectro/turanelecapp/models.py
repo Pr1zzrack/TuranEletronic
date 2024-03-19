12a38b72cb02fd5d3d31f227e293f464f8e81384
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-19 19:46:35 +0600

    Update models.py

--- turanelectro/turanelecapp/models.py
+++ turanelectro/turanelecapp/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+from django.db import models
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    ordered_product_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Categories(models.Model):
     name = models.CharField(max_length=64, verbose_name='Категория')
 
