a3dfdba6c27cddce96364780a74dcdafc1d7f6e4
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-19 20:39:22 +0600

    Update views.py

--- turanelectro/turanelecapp/views.py
+++ turanelectro/turanelecapp/views.py
@@ -14,7 +14,7 @@ from rest_framework import generics
 from .models import Contact
 from .serializers import OrderSerializer
 
-class OrderCreateView(generics.RetrieveUpdateAPIView):
+class OrderCreateRetrieveView(generics.RetrieveUpdateAPIView):
     queryset = Contact.objects.all()
     serializer_class = OrderSerializer
 
