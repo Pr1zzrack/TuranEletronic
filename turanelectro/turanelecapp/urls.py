4269da0457e47d9c8ec49559a508e84e7dc3ee11
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-21 18:59:00 +0600

    Update urls.py

--- turanelectro/turanelecapp/urls.py
+++ turanelectro/turanelecapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout/', LogoutViewSet.as_view({'post': 'logout'}), name='logout'),
     path('forgot-password/', ForgotPasswordViewSet.as_view({'post': 'forgot_password'}), name='forgot_password'),
     path('signup/', SignupViewSet.as_view({'post': 'signup'}), name='signup'),
+    path('categories/', CategoryViewSet.as_view({'get': 'list'}), name='categories'),
     path('brands/', BrandViewSet.as_view({'get': 'list'}), name='brands'),
     path('products/', ProductViewSet.as_view({'get': 'list'}), name='product_list'),
     path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve',}), name='product_detail'),
