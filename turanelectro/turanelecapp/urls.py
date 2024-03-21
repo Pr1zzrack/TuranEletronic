0ee549499d0c9f1c1e65b45d3df5e5f851ccb464
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-21 18:27:33 +0600

    Update urls.py

--- turanelectro/turanelecapp/urls.py
+++ turanelectro/turanelecapp/urls.py
@@ -4,10 +4,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('login/', AuthViewSet.as_view({'post': 'login'}), name='login'),
-    path('logout/', AuthViewSet.as_view({'post': 'logout'}), name='logout'),
-    path('forgot-password/', AuthViewSet.as_view({'post': 'forgot_password'}), name='forgot_password'),
-    path('signup/', AuthViewSet.as_view({'post': 'signup'}), name='signup'),    path('categories/', CategoryViewSet.as_view({'get': 'list'}), name='categories'),
+    path('login/', LoginViewSet.as_view({'post': 'login'}), name='login'),
+    path('logout/', LogoutViewSet.as_view({'post': 'logout'}), name='logout'),
+    path('forgot-password/', ForgotPasswordViewSet.as_view({'post': 'forgot_password'}), name='forgot_password'),
+    path('signup/', SignupViewSet.as_view({'post': 'signup'}), name='signup'),
     path('brands/', BrandViewSet.as_view({'get': 'list'}), name='brands'),
     path('products/', ProductViewSet.as_view({'get': 'list'}), name='product_list'),
     path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve',}), name='product_detail'),
