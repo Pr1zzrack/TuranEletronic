af6c75a89d13c4749da1f4f2938bb6cd2df14f8c
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-21 16:39:20 +0600

    Update models.py

--- turanelectro/turanelecapp/models.py
+++ turanelectro/turanelecapp/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-from django.db import models
-
 class Contact(models.Model):
     full_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
