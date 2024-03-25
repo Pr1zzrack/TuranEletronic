39409a046bdb69f40673f6761eb34f26a8d2d24b
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-25 13:42:01 +0600

    Update views.py

--- turanelectro/turanelecapp/views.py
+++ turanelectro/turanelecapp/views.py
@@ -159,7 +159,7 @@ class BasketViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # self.perform_create(serializer)
+        self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def list(self, request, *args, **kwargs):
