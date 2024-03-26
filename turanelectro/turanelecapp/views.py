8380bc6a63c4761fb21c7dad0be73b657e55f4cd
Pr1zrack <104278051+Pr1zzrack@users.noreply.github.com>
2024-03-26 16:08:58 +0600

    Update views.py

--- turanelectro/turanelecapp/views.py
+++ turanelectro/turanelecapp/views.py
@@ -102,10 +102,8 @@ class MemoryViewSet(viewsets.ModelViewSet):
 class ProductFilter(FilterSet):
     min_price = NumberFilter(field_name="price", lookup_expr='gte', label='От')
     max_price = NumberFilter(field_name="price", lookup_expr='lte', label='До')
-    color = ModelMultipleChoiceFilter(field_name='color__name', to_field_name='name', queryset=Colors.objects.all(),
-                              label='Цвет')
-    memory = ModelMultipleChoiceFilter(field_name='memory__value', to_field_name='value', queryset=Memories.objects.all(),
-                               label='Память')
+    color = ModelMultipleChoiceFilter(field_name='color__name', to_field_name='name', queryset=Colors.objects.all(), label='Цвет')
+    memory = ModelMultipleChoiceFilter(field_name='memory__value', to_field_name='value', queryset=Memories.objects.all(), label='Память')
 
     class Meta:
         model = Products
@@ -115,7 +113,9 @@ class ProductFilter(FilterSet):
         super().__init__(*args, **kwargs)
         brand_id = self.data.get('brand')
         if brand_id:
-            self.filters['model'].queryset = ProductModel.objects.filter(id=brand_id)
+            # Фильтруем модели по выбранному бренду
+            self.filters['model'].queryset = ProductModel.objects.filter(brand_id=brand_id)
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
